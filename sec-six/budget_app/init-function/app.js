a94ad292c4e803397107572f2b5fcf92bca729ab
DanishKalim1 <engrdanishkalim@gmail.comgit config --global user.email engrdanishkalim@gmail.comgit config --global user.email engrdanishkalim@gmail.com>
2018-04-24 03:45:31 +0500

    In this Lesson I have added all eventlisteners to a single function called setupEventListener

---
+++ sec-six/budget_app/init-function/app.js
@@ -0,0 +1,65 @@
+//Budget controller
+var budgetController = (function(){
+  // some code
+})();
+// UI controller
+var UIController = (function(){
+    var DOMstrings = {
+      inputType:'.add__type',
+      inputDescription: '.add__description',
+      inputValue:'.add__value',
+      inputBtn: '.add__btn',
+
+}
+     return {
+      getInput: function(){
+        return {
+             // getting input data from elements
+              type: document.querySelector(DOMstrings.inputType).value,
+              description: document.querySelector(DOMstrings.inputDescription).value,
+              value: document.querySelector(DOMstrings.inputValue).value
+
+         };
+        
+      },
+      // exposing to public so that we can excess DOMstrings
+      getDOMstrings: function(){
+        return DOMstrings;
+      }
+
+  };
+})();
+
+// Global App controller.
+var controller = (function(budgetctrl,UICtrl){
+// privioulsly all the event listeners were scattered But now i have a seprate function SetupEventListeners  for all events
+var setupEventListeners = function(){
+var DOM = UICtrl.getDOMstrings();
+document.querySelector(DOM.inputBtn).addEventListener('click',ctrlAddItem);
+document.addEventListener('keypress',function(event){
+  if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+};
+
+var ctrlAddItem = function(){
+      // Get the input data.
+      var input = UICtrl.getInput();
+      
+      //add the item to the budget controller
+      // add the item to the Ui
+      // calculate budget
+      // Dispalay budget
+      console.log(input);
+    };
+
+return {
+  init:function(){
+     console.log("Your App Has started");
+    setupEventListeners();
+  }
+};
+})(budgetController,UIController);
+
+controller.init();
